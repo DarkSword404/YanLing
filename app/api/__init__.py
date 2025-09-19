2161917f93e3a8ea22da50dd584e6222aa171926
YanLing Developer <developer@yanling.com>
2025-09-19 11:09:39 +0800

    Clear repository for new CTF platform development

--- app/api/__init__.py
+++
@@ -1,7 +0,0 @@
-# API模块初始化文件
-from .auth import auth_api_bp
-from .challenges import challenges_api_bp
-from .teams import teams_api_bp
-from .admin import admin_api_bp
-
-__all__ = ['auth_api_bp', 'challenges_api_bp', 'teams_api_bp', 'admin_api_bp']
